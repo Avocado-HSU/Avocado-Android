bf32ae1580737d2d2fd38592e0302316d28aeeed
김성민 <arcn1345@gmail.com>
2024-07-09 03:53:57 +0900

    feat: 홈 프래그먼트 UI 연결

--- app/src/main/java/com/example/avocado_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/avocado_android/ui/MainActivity.kt
@@ -5,14 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation.findNavController
 import com.example.avocado_android.R
+import com.example.avocado_android.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,8 +24,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // 바텀 네비게이션 아이템의 원래 색깔 나오게
-        val nav = findViewById<BottomNavigationView>(R.id.main_nav_bar)
-        nav.itemIconTintList = null
+        binding.mainNavBar.itemIconTintList = null
 
     }
 }
