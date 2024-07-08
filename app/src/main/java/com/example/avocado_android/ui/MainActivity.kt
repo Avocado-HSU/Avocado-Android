deeae66a445da4216522c96fe59d2a6b9675bf17
김성민 <arcn1345@gmail.com>
2024-07-09 03:32:18 +0900

    feat: 바텀 네비게이션

--- app/src/main/java/com/example/avocado_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/avocado_android/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.avocado_android.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 바텀 네비게이션 아이템의 원래 색깔 나오게
+        val nav = findViewById<BottomNavigationView>(R.id.main_nav_bar)
+        nav.itemIconTintList = null
+
     }
 }
---
+++ app/src/main/java/com/example/avocado_android/ui/home/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.example.avocado_android.ui.home
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment()  {
+}
