2f341ba3dfd163cda4da71c5537c86660706b6b3
김성민 <arcn1345@gmail.com>
2024-07-10 02:41:10 +0900

    feat: BottomNavigation, Fragment 연결

--- app/src/main/java/com/example/avocado_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/avocado_android/ui/MainActivity.kt
@@ -5,26 +5,86 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.avocado_android.R
+import com.example.avocado_android.base.BaseActivity
 import com.example.avocado_android.databinding.ActivityMainBinding
+import com.example.avocado_android.ui.home.HomeFragment
+import com.example.avocado_android.ui.library.LibraryFragment
+import com.example.avocado_android.ui.quiz.QuizFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-        // 바텀 네비게이션 아이템의 원래 색깔 나오게
-        binding.mainNavBar.itemIconTintList = null
+    private lateinit var navController: NavController
 
+    private var homeFragment: HomeFragment? = null
+    private var libraryFragment: LibraryFragment? = null
+    private var quizFragment:QuizFragment? = null
+
+    override fun setLayout() {
+        setBottomNavigation()
     }
+
+    private fun setBottomNavigation() {
+        // 최소 실행시 프래그먼트 설정
+        binding.mainNavBar.selectedItemId = R.id.homeFragment
+        homeFragment = HomeFragment()
+        changeFragment(homeFragment!!)
+
+        binding.mainNavBar.itemIconTintList = null
+        binding.mainNavBar.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.homeFragment -> {
+                    if (homeFragment == null) {
+                        homeFragment = HomeFragment()
+                        addFragment(homeFragment!!)
+                    }
+                    if (homeFragment != null) supportFragmentManager.beginTransaction().apply { show(homeFragment!!).commit() }
+                    if (libraryFragment != null) supportFragmentManager.beginTransaction().apply { hide(libraryFragment!!).commit() }
+                    if (quizFragment != null) supportFragmentManager.beginTransaction().apply { hide(quizFragment!!).commit() }
+                    return@setOnItemSelectedListener true
+                }
+                R.id.libraryFragment -> {
+                    if (libraryFragment == null) {
+                        libraryFragment = LibraryFragment()
+                        addFragment(libraryFragment!!)
+                    }
+                    if (libraryFragment != null) supportFragmentManager.beginTransaction().apply { show(libraryFragment!!).commit() }
+                    if (homeFragment != null) supportFragmentManager.beginTransaction().apply { hide(homeFragment!!).commit() }
+                    if (quizFragment != null) supportFragmentManager.beginTransaction().apply { hide(quizFragment!!).commit() }
+                    return@setOnItemSelectedListener true
+                }
+                R.id.quizFragment -> {
+                    if (quizFragment == null) {
+                        quizFragment = QuizFragment()
+                        addFragment(quizFragment!!)
+                    }
+                    if (quizFragment != null) supportFragmentManager.beginTransaction().apply { show(quizFragment!!).commit() }
+                    if (homeFragment != null) supportFragmentManager.beginTransaction().apply { hide(homeFragment!!).commit() }
+                    if (libraryFragment != null) supportFragmentManager.beginTransaction().apply { hide(libraryFragment!!).commit() }
+                    return@setOnItemSelectedListener true
+                }
+                else -> return@setOnItemSelectedListener true
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.mainFragmentContainerView.id, fragment)
+            .commit()
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(binding.mainFragmentContainerView.id, fragment)
+            .commit()
+    }
+
 }
