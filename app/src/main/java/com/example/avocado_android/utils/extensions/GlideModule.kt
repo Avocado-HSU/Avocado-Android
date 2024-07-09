ace71659779df1b669a876da963288484580896b
jiwon <pjw99032986@gmail.com>
2024-07-10 03:06:31 +0900

    feat : 글라이드 drawable 모듈추가

--- app/src/main/java/com/example/avocado_android/utils/extensions/GlideModule.kt
+++ app/src/main/java/com/example/avocado_android/utils/extensions/GlideModule.kt
@@ -1,10 +1,27 @@
 package com.example.avocado_android.utils.extensions
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
 // Glide -> GlideApp로 사용
 @GlideModule
 class GlideModule: AppGlideModule() {
+    @BindingAdapter(value = ["loadUrl"])
+    fun ImageView.loadUrl(url: String?) {
+        url?.let {
+            Glide.with(this.context)
+                .load(url)
+                .into(this)
+        }
+    }
+    @BindingAdapter("drawableRes")
+    fun ImageView.setDrawableRes(drawableRes: Int?) {
+        drawableRes?.let {
+            this.setImageResource(it)
+        }
+    }
 
 }
