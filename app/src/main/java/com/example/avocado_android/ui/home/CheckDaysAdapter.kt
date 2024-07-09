b1177a1c165ab62c4526a80722a68b4c56f9ae4e
jiwon <pjw99032986@gmail.com>
2024-07-10 03:07:49 +0900

    feat : diffutill 추가

---
+++ app/src/main/java/com/example/avocado_android/ui/home/CheckDaysAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.avocado_android.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.avocado_android.R
+import com.example.avocado_android.databinding.ItemHomeCheckedDaysBinding
+import com.example.avocado_android.domain.model.HomeDaysItem
+
+class HomeDaysDiffCallback(
+    private val oldList: List<HomeDaysItem>,
+    private val newList: List<HomeDaysItem>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) = oldList[oldItemPosition].id == newList[newItemPosition].id
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int)= oldList[oldItemPosition] == newList[newItemPosition]
+}
+class CheckDaysAdapter : ListAdapter<HomeDaysItem, CheckDaysAdapter.HomeDaysViewHolder>(HomeDaysDiffCallback()) {
+
+    inner class HomeDaysViewHolder(private val binding: ItemHomeCheckedDaysBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: HomeDaysItem) {
+            binding.checkedDayDate = item
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeDaysViewHolder {
+        val binding: ItemHomeCheckedDaysBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_home_checked_days, parent, false
+        )
+        return HomeDaysViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeDaysViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class HomeDaysDiffCallback : DiffUtil.ItemCallback<HomeDaysItem>() {
+        override fun areItemsTheSame(oldItem: HomeDaysItem, newItem: HomeDaysItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: HomeDaysItem, newItem: HomeDaysItem): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
