2704d29b0af26687ae00bd266af5610c49661fb1
김성민 <arcn1345@gmail.com>
2024-07-11 01:28:59 +0900

    feat: search 프래그먼트 로직 추가

--- app/src/main/java/com/example/avocado_android/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/avocado_android/ui/search/SearchFragment.kt
@@ -1,18 +1,32 @@
 package com.example.avocado_android.ui.search
 
-//import android.os.Bundle
-//import androidx.fragment.app.Fragment
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import com.example.avocado_android.R
-//import com.example.avocado_android.base.BaseFragment
-//import com.example.avocado_android.databinding.FragmentSearchBinding
-//
-//class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
-//
-//    override fun setLayout() {
-//
-//    }
-//
-//}
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.avocado_android.R
+import com.example.avocado_android.base.BaseFragment
+import com.example.avocado_android.databinding.FragmentSearchBinding
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+    private val viewModel : SearchViewModel by activityViewModels()
+    private lateinit var recentWordAdapter: RecentWordAdapter
+
+    override fun setLayout() {
+
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+        recentWordAdapter = RecentWordAdapter()
+        binding.searchRecentWordRv.adapter = recentWordAdapter
+
+        viewModel.recentWordList.observe(viewLifecycleOwner) { newList ->
+            recentWordAdapter.submitList(newList)
+        }
+    }
+
+}
