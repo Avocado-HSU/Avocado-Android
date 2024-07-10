a662f83d6bc10fc98ae3a24124a9474160c213ed
김성민 <arcn1345@gmail.com>
2024-07-10 22:29:45 +0900

    feat: SearchFragment 추가, 이미지 추가

---
+++ app/src/main/java/com/example/avocado_android/ui/search/SearchFragment.kt
@@ -0,0 +1,18 @@
+package com.example.avocado_android.ui.search
+
+//import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import com.example.avocado_android.R
+//import com.example.avocado_android.base.BaseFragment
+//import com.example.avocado_android.databinding.FragmentSearchBinding
+//
+//class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+//
+//    override fun setLayout() {
+//
+//    }
+//
+//}
