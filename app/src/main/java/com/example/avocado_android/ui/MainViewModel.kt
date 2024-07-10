caf316a31c1587f41017ec68d7276cae70490234
jiwon <pjw99032986@gmail.com>
2024-07-11 02:09:21 +0900

    feat : recommended 리스트 추가

--- app/src/main/java/com/example/avocado_android/ui/MainViewModel.kt
+++ app/src/main/java/com/example/avocado_android/ui/MainViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.avocado_android.R
 import com.example.avocado_android.domain.model.HomeDaysItem
+import com.example.avocado_android.domain.model.HomeRecommendedItem
 
 class MainViewModel : ViewModel() {
     private val _checkedDayItems = MutableLiveData<List<HomeDaysItem>>()
@@ -13,6 +14,9 @@ class MainViewModel : ViewModel() {
 
     private val _bestChoiceItem = MutableLiveData<List<String>>()
     val bestChoiceItem: LiveData<List<String>> get() = _bestChoiceItem
+
+    private val _recommendedItem = MutableLiveData<List<HomeRecommendedItem>>()
+    val recommendedItem: LiveData<List<HomeRecommendedItem>> get() = _recommendedItem
 
 
     init {
@@ -34,6 +38,14 @@ class MainViewModel : ViewModel() {
             "Food",
             "Responsive"
         )
+
+        _recommendedItem.value = listOf(
+            HomeRecommendedItem(0,"Iconic","상징적인","icon(상징) + -ic(~의, ~적인)"),
+            HomeRecommendedItem(1,"Friendship","우정","Friend(친구) + -ship(~관계, ~상태)"),
+            HomeRecommendedItem(2,"Childhood","어린시절","Child(아이) + -hood(~기, ~상태)"),
+            HomeRecommendedItem(3,"Childhood","어린시절","Child(아이) + -hood(~기, ~상태)")
+        )
+
     }
 
     fun updateData(pos: Int, newData: HomeDaysItem) {
