e6b0d83c7cbfdcaf671dd4df3dc6f268846100b6
김성민 <arcn1345@gmail.com>
2024-07-10 22:36:38 +0900

    feat: 확인용 LibraryFragment -> SearchFragment UI 적용

--- app/src/main/java/com/example/avocado_android/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/avocado_android/ui/library/LibraryFragment.kt
@@ -1,6 +1,20 @@
 package com.example.avocado_android.ui.library
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.avocado_android.R
 
-class LibraryFragment : Fragment()  {
+class LibraryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // 레이아웃을 인플레이트하여 반환
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
 }
