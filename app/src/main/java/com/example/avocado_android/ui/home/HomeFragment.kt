754f16448ece40e9edd92cfe4d0919fbf5f931c7
김성민 <arcn1345@gmail.com>
2024-07-11 01:39:24 +0900

    feat: 홈 프래그먼트 검색 EditText -> TextView로 변경

--- app/src/main/java/com/example/avocado_android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/avocado_android/ui/home/HomeFragment.kt
@@ -7,18 +7,28 @@ import com.example.avocado_android.R
 import com.example.avocado_android.base.BaseFragment
 import com.example.avocado_android.databinding.FragmentHomeBinding
 import com.example.avocado_android.ui.MainViewModel
+import com.example.avocado_android.ui.search.SearchFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home)  {
     private val viewModel : MainViewModel by activityViewModels()
     private lateinit var adapter: CheckDaysAdapter
     override fun setLayout() {
         initAdapter()
+        setSearchBar()
     }
     private fun initAdapter() {
         adapter = CheckDaysAdapter()
         binding.homeDaysRv.adapter = adapter
         viewModel.homeDaysItem.observe(viewLifecycleOwner) { newList ->
             adapter.submitList(newList)
+        }
+    }
+
+    private fun setSearchBar() {
+        binding.homeSearchHereEt.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .add(R.id.main_fragment_container_view, SearchFragment())
+                .commitAllowingStateLoss()
         }
     }
 }
