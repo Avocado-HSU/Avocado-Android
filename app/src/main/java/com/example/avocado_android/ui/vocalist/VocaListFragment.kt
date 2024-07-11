95cdb406847186aade763447cc75b9dd04182d27
Seongmin <arcn1345@gmail.com>
2024-07-11 20:15:27 +0900

    feat: VocaListFragment 추가

---
+++ app/src/main/java/com/example/avocado_android/ui/vocalist/VocaListFragment.kt
@@ -0,0 +1,12 @@
+package com.example.avocado_android.ui.vocalist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.avocado_android.R
+
+class VocaListFragment : Fragment() {
+
+}
