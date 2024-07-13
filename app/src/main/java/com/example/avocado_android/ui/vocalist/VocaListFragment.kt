891d2e663f64a825a172764e48c47f2e3ddad80a
Seongmin <arcn1345@gmail.com>
2024-07-13 15:38:19 +0900

    feat: VocaListFragment 디자인

--- app/src/main/java/com/example/avocado_android/ui/vocalist/VocaListFragment.kt
+++ app/src/main/java/com/example/avocado_android/ui/vocalist/VocaListFragment.kt
@@ -5,8 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.avocado_android.R
+import com.example.avocado_android.base.BaseFragment
+import com.example.avocado_android.databinding.FragmentVocaListBinding
 
-class VocaListFragment : Fragment() {
+class VocaListFragment : BaseFragment<FragmentVocaListBinding>(R.layout.fragment_voca_list) {
+    override fun setLayout() {
+        toast()
+    }
+
+    fun toast() {
+        Toast.makeText(requireContext(), "gagag", Toast.LENGTH_SHORT).show()
+    }
 
 }
