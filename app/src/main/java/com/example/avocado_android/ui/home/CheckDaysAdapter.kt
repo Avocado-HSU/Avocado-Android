423c9718477c0222f3a06fb9e0ea62d2da4e1750
jiwon <pjw99032986@gmail.com>
2024-07-14 22:02:52 +0900

    refactor : adapter 리펙터링

--- app/src/main/java/com/example/avocado_android/ui/home/CheckDaysAdapter.kt
+++ app/src/main/java/com/example/avocado_android/ui/home/CheckDaysAdapter.kt
@@ -7,37 +7,23 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.avocado_android.R
+import com.example.avocado_android.base.BaseAdapter
+import com.example.avocado_android.base.BaseDiffCallback
 import com.example.avocado_android.databinding.ItemHomeCheckedDaysBinding
 import com.example.avocado_android.domain.model.home.HomeDaysItem
 
-class CheckDaysAdapter : ListAdapter<HomeDaysItem, CheckDaysAdapter.HomeDaysViewHolder>(HomeDaysDiffCallback()) {
 
-    inner class HomeDaysViewHolder(private val binding: ItemHomeCheckedDaysBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: HomeDaysItem) {
-            binding.checkedDayDate = item
-            binding.executePendingBindings()
-        }
-    }
+class CheckDaysAdapter : BaseAdapter<HomeDaysItem, ItemHomeCheckedDaysBinding>(
+    BaseDiffCallback(
+        areItemsTheSame = { oldItem, newItem -> oldItem.id == newItem.id },
+        areContentsTheSame = { oldItem, newItem -> oldItem == newItem }
+    )
+) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeDaysViewHolder {
-        val binding: ItemHomeCheckedDaysBinding = DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context),
-            R.layout.item_home_checked_days, parent, false
-        )
-        return HomeDaysViewHolder(binding)
-    }
+    override val layoutId: Int
+        get() = R.layout.item_home_checked_days
 
-    override fun onBindViewHolder(holder: HomeDaysViewHolder, position: Int) {
-        holder.bind(getItem(position))
-    }
-
-    class HomeDaysDiffCallback : DiffUtil.ItemCallback<HomeDaysItem>() {
-        override fun areItemsTheSame(oldItem: HomeDaysItem, newItem: HomeDaysItem): Boolean {
-            return oldItem.id == newItem.id
-        }
-
-        override fun areContentsTheSame(oldItem: HomeDaysItem, newItem: HomeDaysItem): Boolean {
-            return oldItem == newItem
-        }
+    override fun bind(binding: ItemHomeCheckedDaysBinding, item: HomeDaysItem) {
+        binding.checkedDayDate = item
     }
 }
