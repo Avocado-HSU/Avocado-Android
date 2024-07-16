0394e24e69f6d868b90c17169c9a7daefdb51447
김성민 <arcn1345@gmail.com>
2024-07-16 20:57:15 +0900

    feat: ChatBotFragment 어댑터 연결 및 EditText 입력 완료 (엔터) -> 키보드 숨기기

--- app/src/main/java/com/example/avocado_android/ui/ChatBot/ChatBotFragment.kt
+++ app/src/main/java/com/example/avocado_android/ui/ChatBot/ChatBotFragment.kt
@@ -1,8 +1,15 @@
 package com.example.avocado_android.ui.ChatBot
 
+import android.app.Activity
+import android.content.Context
 import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.avocado_android.R
@@ -27,17 +34,41 @@ class ChatBotFragment : BaseFragment<FragmentChatbotBinding>(R.layout.fragment_c
         adapter = ChatBotAdapter()
         binding.chatbotChatRv.adapter = adapter
 
-//        viewModel.userMsgList.observe(viewLifecycleOwner) { userMsgList ->
-//            adapter.submitList(userMsgList)
-//        }
-
+        // 챗봇
         viewModel.botMsgList.observe(viewLifecycleOwner) { botMsgList ->
-            Log.d("로그", "DDdd")
-            adapter.submitList(botMsgList)
+            val currentList = adapter.currentList.toMutableList()  // 기존 데이터를 가져옴
+            currentList.addAll(botMsgList) // 새로운 데이터를 기존 데이터에 추가
+            adapter.submitList(currentList) // 변경된 리스트를 Adapter에 전달
         }
 
-//        viewModel.imageCardList.observe(viewLifecycleOwner) { imageCardList ->
-//            adapter.submitList(imageCardList)
-//        }
+        // 사용자
+        binding.chatbotChatEt.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                viewModel.userMsgList.observe(viewLifecycleOwner) { userMsgList ->
+                    val currentList = adapter.currentList.toMutableList() // 기존 데이터를 가져옴
+                    currentList.addAll(userMsgList) // 새로운 데이터를 기존 데이터에 추가
+                    adapter.submitList(currentList) // 변경된 리스트를 Adapter에 전달
+
+                    // 키보드 내리고 내용 삭제
+                    hideKeyboard(binding.chatbotChatEt, requireContext())
+                    binding.chatbotChatEt.text = null
+                }
+                true
+            } else {
+                false
+            }
+        }
+
+        // 이미지
+        //viewModel.imageCardList.observe(viewLifecycleOwner) { imageCardList ->
+        //    adapter.submitList(imageCardList)
+        //}
     }
+
+    // 화면 터치 시 키보드 내리기
+    private fun hideKeyboard(view: View, context: Context) {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
