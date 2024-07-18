8adc69314cb4f6c938c779bc4cfe01523ef63f81
김성민 <arcn1345@gmail.com>
2024-07-18 23:08:30 +0900

    Reactor: 챗봇 liveData -> Flow 마이그레이션

--- app/src/main/java/com/example/avocado_android/ui/ChatBot/ChatBotFragment.kt
+++ app/src/main/java/com/example/avocado_android/ui/ChatBot/ChatBotFragment.kt
@@ -12,10 +12,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.avocado_android.R
 import com.example.avocado_android.base.BaseFragment
 import com.example.avocado_android.databinding.FragmentChatbotBinding
 import com.example.avocado_android.ui.vocalist.VocaListViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class ChatBotFragment : BaseFragment<FragmentChatbotBinding>(R.layout.fragment_chatbot) {
 
@@ -34,35 +39,47 @@ class ChatBotFragment : BaseFragment<FragmentChatbotBinding>(R.layout.fragment_c
         adapter = ChatBotAdapter()
         binding.chatbotChatRv.adapter = adapter
 
-        // 챗봇
-        viewModel.botMsgList.observe(viewLifecycleOwner) { botMsgList ->
-            val currentList = adapter.currentList.toMutableList()  // 기존 데이터를 가져옴
-            currentList.addAll(botMsgList) // 새로운 데이터를 기존 데이터에 추가
-            adapter.submitList(currentList) // 변경된 리스트를 Adapter에 전달
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                launch {
+                    // 챗봇
+                    viewModel.botMsgList.collect { botMsgList ->
+                        val currentList = adapter.currentList.toMutableList()  // 기존 데이터를 가져옴
+                        currentList.addAll(botMsgList) // 새로운 데이터를 기존 데이터에 추가
+                        adapter.submitList(currentList) // 변경된 리스트를 Adapter에 전달
+                    }
+                }
+
+//                launch {
+//                    // 이미지
+//                    viewModel.imageCardList.collect() { imageCardList ->
+//                        adapter.submitList(imageCardList)
+//                    }
+                }
         }
 
         // 사용자
         binding.chatbotChatEt.setOnKeyListener { _, keyCode, event ->
             if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                viewModel.userMsgList.observe(viewLifecycleOwner) { userMsgList ->
-                    val currentList = adapter.currentList.toMutableList() // 기존 데이터를 가져옴
-                    currentList.addAll(userMsgList) // 새로운 데이터를 기존 데이터에 추가
-                    adapter.submitList(currentList) // 변경된 리스트를 Adapter에 전달
+                lifecycleScope.launch {
+                    lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                        viewModel.userMsgList.collect { userMsgList ->
+                            val currentList = adapter.currentList.toMutableList()
+                            currentList.addAll(userMsgList)
+                            adapter.submitList(currentList)
 
-                    // 키보드 내리고 내용 삭제
-                    hideKeyboard(binding.chatbotChatEt, requireContext())
-                    binding.chatbotChatEt.text = null
+                            // 키보드 내리고 내용 삭제
+                            hideKeyboard(binding.chatbotChatEt, requireContext())
+                            binding.chatbotChatEt.text = null
+                        }
+                    }
                 }
                 true
             } else {
                 false
             }
         }
-
-        // 이미지
-        //viewModel.imageCardList.observe(viewLifecycleOwner) { imageCardList ->
-        //    adapter.submitList(imageCardList)
-        //}
     }
 
     // 화면 터치 시 키보드 내리기
