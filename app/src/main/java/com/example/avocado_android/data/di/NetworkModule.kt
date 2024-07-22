8f7ff1ace954bbacfcb2582ef2b57909cd0caf64
Seongmin <arcn1345@gmail.com>
2024-07-22 19:17:56 +0900

    feat: NetworkModule에 AccessTokenIntercepto 싱글톤으로 생성하여 OkHttp에 인터셉터추가 (add)

--- app/src/main/java/com/example/avocado_android/data/di/NetworkModule.kt
+++ app/src/main/java/com/example/avocado_android/data/di/NetworkModule.kt
@@ -1,5 +1,7 @@
 package com.example.avocado_android.data.di
 
+import com.example.avocado_android.utils.token.AccessTokenInterceptor
+import com.example.avocado_android.utils.token.TokenManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -9,6 +11,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
@@ -20,12 +23,18 @@ object NetworkModule {
     @Retention(AnnotationRetention.BINARY)
     annotation class ApiRetrofit
 
+    // HttpLoggingInterceptor 생성
     @Singleton
     @Provides
     fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
         return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
     }
 
+    @Singleton
+    @Provides
+    fun provideAccessTokenInterceptor(tokenManager: TokenManager): AccessTokenInterceptor {
+        return AccessTokenInterceptor(tokenManager)
+    }
     @Singleton
     @Provides
     fun provideMoshiConverterFactory(): MoshiConverterFactory {
@@ -35,9 +44,15 @@ object NetworkModule {
     // OkHttpClient 생성
     @Singleton
     @Provides
-    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        accessTokenInterceptor: AccessTokenInterceptor): OkHttpClient {
         return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
             .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(accessTokenInterceptor)
             .build()
     }
 
