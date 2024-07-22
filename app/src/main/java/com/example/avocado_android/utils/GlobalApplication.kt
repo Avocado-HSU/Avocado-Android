65f6dfdc318401ada6a1d07747cccb14ea3aedba
Seongmin <arcn1345@gmail.com>
2024-07-22 15:30:59 +0900

    feat: Application 클래스 @HiltAndroidApp 추가

--- app/src/main/java/com/example/avocado_android/utils/GlobalApplication.kt
+++ app/src/main/java/com/example/avocado_android/utils/GlobalApplication.kt
@@ -1,7 +1,9 @@
 package com.example.avocado_android.utils
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class GlobalApplication: Application() {
 
 }
