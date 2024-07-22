36ca16ec3c9faf8fcd2eb69f9c99ea4a38e35942
Seongmin <arcn1345@gmail.com>
2024-07-22 17:13:39 +0900

    Reactor: 네트워크 모듈 이름 변경

--- app/src/main/java/com/example/avocado_android/data/di/NetworkModule.kt
+++ app/src/main/java/com/example/avocado_android/data/di/NetworkModule.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetWorkModule {
+object NetworkModule {
 
     @Qualifier
     @Retention(AnnotationRetention.BINARY)
