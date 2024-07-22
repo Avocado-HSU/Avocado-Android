c018e512552bdc3048fa0d28e0fe14fa9b5695ec
Seongmin <arcn1345@gmail.com>
2024-07-22 17:33:19 +0900

    feat: RepositoryModule 추가 (챗봇만 추가됨, 나머지는 구현후 추가)

--- app/src/main/java/com/example/avocado_android/data/di/RepositoryModule.kt
+++ app/src/main/java/com/example/avocado_android/data/di/RepositoryModule.kt
@@ -1,11 +1,22 @@
 package com.example.avocado_android.data.di
 
+import com.example.avocado_android.data.source.chatbot.ChatBotDataSource
+import com.example.avocado_android.data.source.chatbot.ChatBotRepositoryImpl
+import com.example.avocado_android.domain.repository.ChatBotRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
 
+    @Singleton
+    @Provides
+    fun provideChatBotRepository(chatBotDataSource: ChatBotDataSource): ChatBotRepository =
+        ChatBotRepositoryImpl(chatBotDataSource)
+
+    // 이후 리포지토리 추가하고 추가하면 됨
 }
