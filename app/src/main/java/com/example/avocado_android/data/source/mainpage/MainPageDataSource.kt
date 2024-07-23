310234f3d2505daf99a89b315db7f104375ad6e5
김성민 <arcn1345@gmail.com>
2024-07-23 20:43:52 +0900

    feat: MainPageApi -> 날짜(date) : @Body MainPageRequestDto 추가

--- app/src/main/java/com/example/avocado_android/data/source/mainpage/MainPageDataSource.kt
+++ app/src/main/java/com/example/avocado_android/data/source/mainpage/MainPageDataSource.kt
@@ -2,6 +2,7 @@ package com.example.avocado_android.data.source.mainpage
 
 import android.util.Log
 import com.example.avocado_android.data.remote.MainPageApi
+import com.example.avocado_android.domain.model.request.MainPageRequestDto
 import com.example.avocado_android.domain.model.response.RecentSearchWordResponseDto
 import com.example.avocado_android.domain.model.response.main.MainPageResponseDto
 import com.example.avocado_android.domain.model.response.main.SearchWordResponseDto
@@ -14,8 +15,8 @@ class MainPageDataSource @Inject constructor(
     private val mainPageApi: MainPageApi
 ) {
 
-    fun getMainPage(id: Long) : Flow<MainPageResponseDto> = flow {
-        val result = mainPageApi.getMainPage(id)
+    fun getMainPage(id: Long, date: MainPageRequestDto) : Flow<MainPageResponseDto> = flow {
+        val result = mainPageApi.getMainPage(id, date)
         emit(result)
     }.catch {
         Log.e("GET getMainPage By MainPageResponseDto Failure", it.message.toString()) }
