4c7bc022d576cb8f9a031d8b0131ef1c8e151282
jiwon <pjw99032986@gmail.com>
2024-07-24 14:23:25 +0900

    feat: 리스폰스 데이터 클래스 추가

--- app/src/main/java/com/example/avocado_android/domain/model/response/main/PopularWordDto.kt
+++ app/src/main/java/com/example/avocado_android/domain/model/response/main/PopularWordDto.kt
@@ -1,5 +1,5 @@
 package com.example.avocado_android.domain.model.response.main
 
 data class PopularWordDto(
-    val popularWords: List<String>
+    val popularWords: List<String> = listOf()
 )
--- app/src/main/java/com/example/avocado_android/domain/model/response/main/SearchWordResponseDto.kt
+++ app/src/main/java/com/example/avocado_android/domain/model/response/main/SearchWordResponseDto.kt
@@ -1,8 +1,8 @@
 package com.example.avocado_android.domain.model.response.main
 
 data class SearchWordResponseDto (
-    val isSuccess : Boolean,
-    val isLibraryRegistered: Boolean,
-    val libraryId: Int,
-    val characterImgUrl: String
+    val isSuccess : Boolean = false,
+    val isLibraryRegistered: Boolean = false,
+    val libraryId: Int = 0,
+    val characterImgUrl: String = ""
 )
