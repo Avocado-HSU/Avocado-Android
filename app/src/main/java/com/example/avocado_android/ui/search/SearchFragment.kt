7e2ec2ddff7ee02bcbc58a6a84f965c4cac1e63b
김성민 <arcn1345@gmail.com>
2024-07-25 03:08:03 +0900

    fix: SearchFragment 로직 일부 수정 (WordListFragment와 뷰모델 공유하여 값 공유)

--- app/src/main/java/com/example/avocado_android/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/avocado_android/ui/search/SearchFragment.kt
@@ -8,18 +8,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.avocado_android.R
 import com.example.avocado_android.base.BaseFragment
 import com.example.avocado_android.databinding.FragmentSearchBinding
+import com.example.avocado_android.domain.model.response.main.SearchWordResponseDto
 import com.example.avocado_android.ui.home.HomeFragmentDirections
+import com.example.avocado_android.ui.vocalist.AffixAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
 
     private val viewModel : SearchViewModel by activityViewModels()
     private lateinit var recentWordAdapter: RecentWordAdapter
+    private lateinit var affixAdapter: AffixAdapter
+
+    private lateinit var query: String
 
     override fun setLayout() {
         setAdapter()
@@ -30,19 +38,28 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
         recentWordAdapter = RecentWordAdapter()
         binding.searchRecentWordRv.adapter = recentWordAdapter
 
-       /* viewModel.recentWordList.observe(viewLifecycleOwner) { newList ->
-            recentWordAdapter.submitList(newList)
-        }*/
+        //affixAdapter = AffixAdapter()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.searchWordResponseDto.collect() { data ->
+                //affixAdapter.submitList(data)
+            }
+        }
+
+
     }
 
+    // 검색 창에 단어 입력하고 엔터 누르면 서버에서 값 받아와 뷰모델로 저장
     private fun searchWord() {
         binding.searchSearchBar.setOnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH ||
                     event?.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER) {
-                val query = binding.searchSearchBar.text.toString()
-                viewModel.wordSearch(0, query)
-                val action = SearchFragmentDirections.actionSearchFragmentToVocaListFragment()
+                query = binding.searchSearchBar.text.toString()
+
+                viewModel.wordSearch(14, query)
+                val action = SearchFragmentDirections.actionSearchFragmentToWordListFragment(query)
                 findNavController().navigate(action)
+
                 true
             } else {
                 false
