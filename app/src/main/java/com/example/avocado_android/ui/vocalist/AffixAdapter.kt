c8fa08403eb5756971c0892f4da960b4dff3cc81
김성민 <arcn1345@gmail.com>
2024-07-25 03:06:15 +0900

    fix: AffixAdapter -> 아이템 layout 이름 변경

--- app/src/main/java/com/example/avocado_android/ui/vocalist/AffixAdapter.kt
+++ app/src/main/java/com/example/avocado_android/ui/vocalist/AffixAdapter.kt
@@ -1,31 +1,22 @@
 package com.example.avocado_android.ui.vocalist
 
-import android.content.ClipData.Item
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.example.avocado_android.R
 import com.example.avocado_android.base.BaseAdapter
 import com.example.avocado_android.base.BaseDiffCallback
-import com.example.avocado_android.databinding.ItemSameWordBinding
-import com.example.avocado_android.databinding.ItemVocalistAffixBinding
-import com.example.avocado_android.domain.model.local.vocalist.AffixItem
-import com.example.avocado_android.domain.model.local.vocalist.SameWordItem
+import com.example.avocado_android.databinding.ItemWordListAffixBinding
+import com.example.avocado_android.domain.model.response.main.SearchWordResponseDto
 
-class AffixAdapter: BaseAdapter<AffixItem, ItemVocalistAffixBinding>(
+class AffixAdapter: BaseAdapter<SearchWordResponseDto, ItemWordListAffixBinding>(
     BaseDiffCallback(
-        itemsTheSame = { oldItem, newItem -> oldItem.id == newItem.id },
+        itemsTheSame = { oldItem, newItem -> oldItem == newItem },
         contentsTheSame = { oldItem, newItem -> oldItem == newItem }
     )
 ) {
 
-    override val layoutId: Int = R.layout.item_vocalist_affix
+    override val layoutId: Int = R.layout.item_word_list_affix
 
-    override fun bind(binding: ItemVocalistAffixBinding, item: AffixItem) {
-        binding.affixItem = item
+    override fun bind(binding: ItemWordListAffixBinding, item: SearchWordResponseDto) {
+        binding.searchWordResponseDto = item
     }
 
 }
