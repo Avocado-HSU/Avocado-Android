c6714e0c3074bca6eaec3cefdc2c9fb21eb271cd
jiwon <pjw99032986@gmail.com>
2024-07-25 02:45:41 +0900

    refactor : 라이브러리 뷰모델

---
+++ app/src/main/java/com/example/avocado_android/ui/library/LibraryViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.avocado_android.ui.library
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.avocado_android.domain.model.local.library.LibraryWordCard
+import com.example.avocado_android.domain.model.request.MainPageRequestDto
+import com.example.avocado_android.domain.model.response.library.LibraryPageResponseDto
+import com.example.avocado_android.domain.model.response.library.LibraryWordDto
+import com.example.avocado_android.domain.repository.librarypage.LibraryPageRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LibraryViewModel @Inject constructor(
+    private val libraryPageRepository: LibraryPageRepository
+) : ViewModel(){
+    private val _wordCardData = MutableStateFlow(LibraryPageResponseDto())
+    val wordCardData : StateFlow<LibraryPageResponseDto> = _wordCardData
+
+    fun getLibraryData(id: Long) {
+        viewModelScope.launch {
+            try {
+                libraryPageRepository.getLibraryPage(id).collect {
+                    _wordCardData.value = it
+                }
+            } catch (e: Exception) {
+                Log.e("Get MemberData is Error", e.message.toString())
+            }
+        }
+    }
+
+
+}
