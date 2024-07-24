229477630c4ad2e7b06e24fedf370cb974a1ffed
jiwon <pjw99032986@gmail.com>
2024-07-24 14:23:07 +0900

    feat: 텍스트모듈

---
+++ app/src/main/java/com/example/avocado_android/utils/extensions/TextModule.kt
@@ -0,0 +1,13 @@
+package com.example.avocado_android.utils.extensions
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+object TextModule {
+
+
+}
