ecc0ffb38a427a50b3df8c789f32b8410f80425f
김성민 <arcn1345@gmail.com>
2024-07-26 01:37:27 +0900

    feat: 검색화면에 로그인 id 가져오는 함수 추가

--- app/src/main/java/com/example/avocado_android/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/avocado_android/ui/search/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.example.avocado_android.ui.search
 
 import android.content.Context
+import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
@@ -12,10 +13,13 @@ import androidx.navigation.fragment.findNavController
 import com.example.avocado_android.R
 import com.example.avocado_android.base.BaseFragment
 import com.example.avocado_android.databinding.FragmentSearchBinding
+import com.example.avocado_android.ui.login.LoginViewModel
 import com.example.avocado_android.ui.vocalist.PrefixAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
@@ -23,6 +27,8 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
     private val viewModel : SearchViewModel by activityViewModels()
     private lateinit var recentWordAdapter: RecentWordAdapter
     private lateinit var prefixAdapter: PrefixAdapter
+
+    private val loginViewModel: LoginViewModel by activityViewModels()
 
     private lateinit var query: String
 
@@ -42,8 +48,19 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
                 //affixAdapter.submitList(data)
             }
         }
+    }
 
-
+    private fun getLoginId() : Long {
+        var id: Long = 0L
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                loginViewModel.memberData.collectLatest { response ->
+                    Log.d("getLoginId", response.id)
+                    id = response.id.takeIf { it.isNotEmpty() }?.toLong() ?: 0L
+                }
+            }
+        }
+        return id
     }
 
     // 검색 창에 단어 입력하고 엔터 누르면 서버에서 값 받아와 뷰모델로 저장
@@ -57,7 +74,7 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
                 imm.hideSoftInputFromWindow(v.windowToken, 0)
 
                 // 서버 요청
-                viewModel.wordSearch(14, query)
+                viewModel.wordSearch(getLoginId(), query)
 
                 // 데이터가 준비된 후 화면 전환
                 viewLifecycleOwner.lifecycleScope.launch {
