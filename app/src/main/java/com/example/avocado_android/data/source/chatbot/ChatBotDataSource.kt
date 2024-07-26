c69e19e9ee54dea7cd02db59faaa3345b044adc8
김성민 <arcn1345@gmail.com>
2024-07-26 18:03:14 +0900

    feat: 챗봇 API 추가

--- app/src/main/java/com/example/avocado_android/data/source/chatbot/ChatBotDataSource.kt
+++ app/src/main/java/com/example/avocado_android/data/source/chatbot/ChatBotDataSource.kt
@@ -3,6 +3,9 @@ package com.example.avocado_android.data.source.chatbot
 import android.util.Log
 import com.example.avocado_android.data.remote.ChatBotApi
 import com.example.avocado_android.domain.model.response.chatbot.ChatBotResponseDto
+import com.example.avocado_android.domain.model.response.chatbot.WordSimilarDto
+import com.example.avocado_android.domain.model.response.search.WordEtymologyDto
+import com.example.avocado_android.domain.model.response.search.WordTipsDto
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
@@ -11,10 +14,28 @@ import javax.inject.Inject
 class ChatBotDataSource @Inject constructor(
     private val chatBotApi: ChatBotApi
 ) {
-    fun requestChatbot(requestType: String, word: String) : Flow<ChatBotResponseDto> = flow {
-        val result = chatBotApi.requestChatbot(requestType, word)
+    fun getWordTips(requestType: String, word: String): Flow<WordTipsDto>  = flow {
+        val result = chatBotApi.getWordTips(requestType, word)
         emit(result)
     }.catch {
-        Log.e("Post requestChatbot By ChatBotResponseDto Failure", it.message.toString())
+        Log.e("Post getWordTips Failure", it.message.toString())
+    }
+    fun getWordSimilar(requestType: String, word: String): Flow<WordSimilarDto> = flow {
+        val result = chatBotApi.getWordSimilar(requestType, word)
+        emit(result)
+    }.catch {
+        Log.e("Post getWordSimilar Failure", it.message.toString())
+    }
+    fun getWordMean(requestType: String, word: String): Flow<WordEtymologyDto> = flow {
+        val result = chatBotApi.getWordMean(requestType, word)
+        emit(result)
+    }.catch {
+        Log.e("Post getWordMean Failure", it.message.toString())
+    }
+    fun getWordEtymology(requestType: String, word: String): Flow<ChatBotResponseDto> = flow {
+        val result = chatBotApi.getWordEtymology(requestType, word)
+        emit(result)
+    }.catch {
+        Log.e("Post getWordEtymology Failure", it.message.toString())
     }
 }
--- app/src/main/java/com/example/avocado_android/domain/repository/chatbot/ChatBotRepository.kt
+++ app/src/main/java/com/example/avocado_android/domain/repository/chatbot/ChatBotRepository.kt
@@ -1,8 +1,14 @@
 package com.example.avocado_android.domain.repository.chatbot
 
 import com.example.avocado_android.domain.model.response.chatbot.ChatBotResponseDto
+import com.example.avocado_android.domain.model.response.chatbot.WordSimilarDto
+import com.example.avocado_android.domain.model.response.search.WordEtymologyDto
+import com.example.avocado_android.domain.model.response.search.WordTipsDto
 import kotlinx.coroutines.flow.Flow
 
 interface ChatBotRepository {
-    suspend fun requestChatbot(requestType: String, word: String) : Flow<ChatBotResponseDto>
+    suspend fun getWordTips(requestType: String, word: String): Flow<WordTipsDto>
+    suspend fun getWordSimilar(requestType: String, word: String): Flow<WordSimilarDto>
+    suspend fun getWordMean(requestType: String, word: String): Flow<WordEtymologyDto>
+    suspend fun getWordEtymology(requestType: String, word: String): Flow<ChatBotResponseDto>
 }
