6be55b7b055eff12a1e658ca29a623aa4d666a23
김성민 <arcn1345@gmail.com>
2024-07-27 22:51:48 +0900

    feat: 네비게이션 충돌 Extension 추가

---
+++ app/src/main/java/com/example/avocado_android/utils/extensions/NavigateSafe.kt
@@ -0,0 +1,20 @@
+package com.example.avocado_android.utils.extensions
+
+import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
+
+fun NavController.navigateSafe(
+    @IdRes resId: Int,
+    args: Bundle? = null,
+    navOptions: NavOptions? = null,
+    navExtras: Navigator.Extras? = null
+) {
+    // 현재 목적지와 이동할 목적지 비교
+    val action = currentDestination?.getAction(resId) ?: graph.getAction(resId)
+    if (action != null && currentDestination?.id != action.destinationId) {
+        navigate(resId, args, navOptions, navExtras)
+    }
+}
