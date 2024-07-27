09864406b9558866435ebc563a111bb6a42d6df5
김성민 <arcn1345@gmail.com>
2024-07-27 19:24:26 +0900

    feat: 팁 이미지 아이템 추가

--- app/src/main/java/com/example/avocado_android/domain/model/local/chatbot/ChatItem.kt
+++ app/src/main/java/com/example/avocado_android/domain/model/local/chatbot/ChatItem.kt
@@ -7,4 +7,6 @@ sealed class ChatItem {
     data class BotChatItem(val msg: String) : ChatItem()
     data class ChatCardListItem(val cards: List<ChatCardItem>) : ChatItem()
     data class ChatCardItem(val chatBotResponseType: ChatBotResponseType?, val imageUri: Int, val text: String) : ChatItem()
+    data class ChatTipsListItem(val cards: List<ChatTipItem>) : ChatItem()
+    data class ChatTipItem(val imageUri: Int, val title: String, val description: String) : ChatItem()
 }
