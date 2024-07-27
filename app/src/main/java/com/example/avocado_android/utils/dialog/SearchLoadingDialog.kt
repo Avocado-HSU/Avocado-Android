d77d7b5d0ec6a7033e0cb9da03c3b246ccadad70
김성민 <arcn1345@gmail.com>
2024-07-27 22:51:35 +0900

    feat: SearchLoadingDialog 추가

---
+++ app/src/main/java/com/example/avocado_android/utils/dialog/SearchLoadingDialog.kt
@@ -0,0 +1,32 @@
+package com.example.avocado_android.utils.dialog
+
+import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import com.example.avocado_android.databinding.DialogSearchLoadingBinding
+
+
+class SearchLoadingDialog: DialogFragment() {
+
+    private lateinit var binding: DialogSearchLoadingBinding
+    var onDismissListener: (() -> Unit)? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DialogSearchLoadingBinding.inflate(inflater, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        dialog?.setCancelable(false)
+
+        return binding.root
+    }
+
+}
